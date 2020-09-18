97a74577ab14f59d4dbf271beed8bbb2603f9550
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-09-18 20:13:22 +0100

    attempt to deploy with simple express server

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
